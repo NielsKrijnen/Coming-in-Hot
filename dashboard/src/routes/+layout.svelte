aa655540b9e701959992976444f87abff577033d
Niels Krijnen <niels.krijnen@icloud.com>
2025-11-05 19:15:15 +0100

    Add sidebar components and integrate with context; update styles and dependencies

--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script lang="ts">
-  import '../app.css';
-  
-  let { children } = $props();
+  import "../app.css"
+  // noinspection ES6UnusedImports
+  import { Sidebar } from "$lib/components/ui"
+
+  let { children } = $props()
 </script>
 
 <svelte:boundary>
-  {@render children()}
+  <Sidebar.Provider class="h-full">
+    <Sidebar.Root>
+      <Sidebar.Content>
+        <Sidebar.Menu>
+          <Sidebar.MenuItem>
+            <Sidebar.MenuButton>
+              {#snippet child({ props })}
+                <a href="/" {...props}>
+                  <span>Dashboard</span>
+                </a>
+              {/snippet}
+            </Sidebar.MenuButton>
+          </Sidebar.MenuItem>
+        </Sidebar.Menu>
+      </Sidebar.Content>
+    </Sidebar.Root>
+    <Sidebar.Inset>
+      <div class="p-4 overflow-y-auto">
+        {@render children()}
+      </div>
+    </Sidebar.Inset>
+  </Sidebar.Provider>
 </svelte:boundary>
