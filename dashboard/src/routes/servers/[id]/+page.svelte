6a9e9af0d4a24f6a1ce0502fe4c161e8a329dd52
Niels Krijnen <niels.krijnen@icloud.com>
2025-12-09 18:21:51 +0100

    Add server stats display and update layout; implement item components for CPU and RAM usage

--- dashboard/src/routes/servers/[id]/+page.svelte
+++ dashboard/src/routes/servers/[id]/+page.svelte
@@ -1,17 +1,63 @@
 <script lang="ts">
+  import { Cpu, MemoryStick } from "@lucide/svelte"
+  import { onMount } from "svelte"
   import * as Servers from "$lib/api/server.remote"
+  // noinspection ES6UnusedImports
+  import { Item, Progress } from "$lib/components/ui"
 
   let { params } = $props()
 
-  const server = await Servers.get(Number(params.id))
-  const env = Object.fromEntries(
-    server.container.Config.Env.map(e => e.split("=", 2))
-  )
+  const stats = $derived(await Servers.getStats(Number(params.id)))
+
+  onMount(() => {
+    const interval = setInterval(async () => {
+      await Servers.getStats(Number(params.id)).refresh()
+    }, 5000)
+
+    return () => clearInterval(interval)
+  })
+
+  export function formatBytes(bytes: number, decimals = 2): string {
+    if (bytes === 0) return "0 B"
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ["B", "KB", "MB", "GB", "TB", "PB"]
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`
+  }
 </script>
 
-{server.name}
+<div class="flex gap-4">
+  <Item.Root variant="outline" class="w-full">
+    <Item.Media variant="icon">
+      <Cpu/>
+    </Item.Media>
+    <Item.Content>
+      <Item.Title>CPU Usage</Item.Title>
+      <Item.Description>
+        <Progress value={stats.cpu}/>
+      </Item.Description>
+    </Item.Content>
+    <Item.Actions>
+      {stats.cpu.toFixed(1)} %
+    </Item.Actions>
+  </Item.Root>
 
-Type: {env.TYPE}
-Version: {env.VERSION}
-
-{JSON.stringify(await Servers.getStats(Number(params.id)))}
+  <Item.Root variant="outline" class="w-full">
+    <Item.Media variant="icon">
+      <MemoryStick/>
+    </Item.Media>
+    <Item.Content>
+      <Item.Title>RAM Usage</Item.Title>
+      <Item.Description>
+        <Progress value={stats.memory.percent}/>
+      </Item.Description>
+    </Item.Content>
+    <Item.Actions>
+      {stats.memory.percent.toFixed(1)} % ({formatBytes(stats.memory.bytes)})
+    </Item.Actions>
+  </Item.Root>
+</div>
